eeba64ef9271b8a623ca17e00b76fc9e9c09f4c0
Aurelien Roux <aureroux13@me.com>
2016-11-10 17:19:04 -0500

    fix add color li function

--- script.js
+++ script.js
@@ -11,13 +11,28 @@ $("#newColor").on("click", function(){
 	$("#colorPanel").toggle();
 })
 
+//refresh span color with sliders
 function changeColor(){
 	var r = $("#red").val();
 	var g = $("#green").val();
 	var b = $("#blue").val();
 	$("span").css("backgroundColor", "rgb(" + r + "," + g + "," + b + ")");
 }
-
 $("input").on("change", changeColor);
+
+
+$("#addColor").on("click", function(){
+	// var newCol = $("span").css("backgroundColor");
+	// $(".palette").append("<li></li>");
+
+	var r = $("#red").val();
+	var g = $("#green").val();
+	var b = $("#blue").val();
+
+	var $colorAdd = $("<li></li>");
+	$(".palette").append($colorAdd);
+	$colorAdd.css("backgroundColor", "rgb(" + r + "," + g + "," + b + ")");
+});
+
 
 }); //end of ready function
